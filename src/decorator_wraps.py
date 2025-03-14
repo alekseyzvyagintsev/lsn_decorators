62336d8cbb2056630f5d480363f9ffa9c6026160
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-03-14 17:47:26 +0300

    созданы дополнительные модули с декораторами

---
+++ src/decorator_wraps.py
@@ -0,0 +1,31 @@
+from functools import wraps # Импортируем декоратор wraps
+
+def checking_that_arg_is(predicate, error_message):
+    def wrapper(function):
+        @wraps(function)
+        def inner(arg):
+            if not predicate(arg):
+                raise ValueError(error_message)
+            return function(arg)
+        return inner
+    return wrapper
+
+
+############################# Предикаты #############################
+def predicate_is_int(x):
+    return type(x) == int
+
+
+def predicate_is_positive(x):
+    return x > 0
+
+
+################ пример использования декоратора wraps ##################
+@checking_that_arg_is(predicate_is_int, "Должно быть целое число")
+@checking_that_arg_is(predicate_is_positive, "Число должно быть больше нуля")
+def square(value):
+    """ Возведение числа в квадрат"""
+    return value ** 2
+
+
+print(help(square))
---
+++ src/decorators_with_args.py
@@ -0,0 +1,72 @@
+from functools import wraps
+
+def checking_that_arg_is(predicate, error_message):
+    def wrapper(function):
+        @wraps(function)
+        def inner(arg):
+            if not predicate(arg):
+                raise ValueError(error_message)
+            return function(arg)
+        return inner
+    return wrapper
+
+
+########################## Предикаты ##########################
+
+def greater_than(value):
+    def predicate(arg):
+        return arg > value
+    return predicate
+
+
+def in_(*values):
+    def predicate(arg):
+        return arg in values
+    return predicate
+
+
+def not_(other_predicate):
+    def predicate(arg):
+        return not other_predicate(arg)
+    return predicate
+
+
+def predicate_is_int(x):
+    return type(x) == int
+
+
+def predicate_is_positive(x):
+    return x > 0
+
+
+######################### пример использования без предикатов ##########################
+
+@checking_that_arg_is(lambda x: x > 0, "Value must be greater than 0!")
+def example_function(value):
+    """ Функция умножения на 2"""
+    return value * 2
+
+print(example_function(1))
+
+########################### пример использования с предикатами ############################
+
+@checking_that_arg_is(greater_than(0), "Non-positive!")
+@checking_that_arg_is(not_(in_(5, 15, 42)), "Bad value!")
+def foo(arg):
+    return arg
+
+
+print(foo(int(example_function(22))))
+
+
+@checking_that_arg_is(predicate_is_int, "Должно быть целое число")
+@checking_that_arg_is(predicate_is_positive, "Число должно быть больше нуля")
+def square(value):
+    """ Возведение числа в квадрат"""
+    return value ** 2
+
+
+print(help(square))
+# print(example_function(1))
+# print(example_function("1"))
+# print(example_function(-1))
